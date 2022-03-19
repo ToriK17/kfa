6a3fe3482ab36b7dd3e862fc1b1b8bdb5fb54651
ToriK17 <vfcad@protonmail.com>
2022-03-19 10:38:15 -0600

    adds sass to project, maps components

---
+++ src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const AboutMe = () => {
+
+	return (
+		<nav>
+			<h1>moreText</h1>
+		</nav>
+	)
+
+}
+export default AboutMe;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,13 @@
-import './App.css';
+import './App.scss';
 import React from 'react';
 import NavBar from '../NavBar/NavBar.tsx';
+import AboutMe from '../AboutMe/AboutMe.tsx';
 
 const App = () => {
   return (
     <div className="App">
-
       <NavBar/>
+      <AboutMe/>
 
     </div>
   );
