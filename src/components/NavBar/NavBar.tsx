d0eb2304c745b1c542d4ee89b8ec12a12f4a2e0f
ToriK17 <vfcad@protonmail.com>
2022-03-26 05:34:40 -0600

    home button renders with appropriate css styles

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,14 +4,22 @@ import './navbar.scss';
 
 
 const NavBar = () => {
-	const Logo =  require('../../assets/icons/favicon copy.ico')
+	// const Logo =  require('../../assets/icons/favicon copy.ico')
 
 	return (
 		<nav className='nav-container'>
 			<div className='inner-container'>
 				<HomeButton/>
+				<div className='nav-link-box'>
+					<button className='nav-link' >Home</button>
+					<button className='nav-link' >Gallery</button>
+					<button className='nav-link' >About</button>
+					<button className='nav-link' >Videos</button>
+					<button className='nav-link' >Contact</button>
+				</div>
+				{/* <a style='nav-link' href='#'>Home</a> */}
 			</div>
-			<img className="nav-img" src={Logo} alt="header logo" />
+			{/* <img className="nav-img" src={Logo} alt="header logo" /> */}
 		</nav>
 	)
 
