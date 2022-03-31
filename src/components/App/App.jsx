3f458404ec95d109bb586196e43c73e94412c084
ToriK17 <vfcad@protonmail.com>
2022-03-31 12:28:39 -0600

    adds initial routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,8 +4,12 @@ import NavBar from '../NavBar/NavBar.tsx';
 import AboutMe from '../AboutMe/AboutMe.tsx';
 
 const App = () => {
+  const background =  require("../../assets/images/canvas_adobe.jpeg")
   return (
     <div className="App">
+      <div className='background'>
+        <img src={background} className='background' alt="background" />
+      </div>
       <NavBar/>
       <AboutMe/>
 
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { NavLink} from "react-router-dom";
 import HomeButton from './HomeButton';
 import './navbar.scss';
 
 
 const NavBar = () => {
-	// const Logo =  require('../../assets/icons/favicon copy.ico')
 
 	return (
+		<>
+
 		<nav className='nav-container'>
-				<HomeButton/>
+
+        <NavLink to='/'><HomeButton/></NavLink>
 			<div className='inner-container'>
 				<div className='nav-links'>
-					<p className='nav-link' >Home</p>
+
+					<p><NavLink to='/' className='nav-link' >Home</NavLink></p>
 					<p className='nav-link' >Gallery</p>
 					<p className='nav-link' >About</p>
 					<p className='nav-link' >Videos</p>
@@ -21,6 +25,7 @@ const NavBar = () => {
 			</div>
 			{/* <img className="nav-img" src={Logo} alt="header logo" /> */}
 		</nav>
+		</>
 	)
 
 }
