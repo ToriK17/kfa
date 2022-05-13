2750bfd7df7b14e6bc1c5befc85781f027497260
ToriK17 <vfcad@protonmail.com>
2022-05-13 17:15:55 -0600

    adds custom nav link for sachi art

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,6 +6,7 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 
 const NavBar = () => {
+	const saLogo =  require("../../assets/icons/sa-logo.png")
 
 	return (
 		<>
@@ -20,6 +21,7 @@ const NavBar = () => {
 					<Link to='/contact' className='nav-link' >Contact</Link>
 					<InstagramIcon sx={{ color: 'white' }}/>
 					<FacebookIcon sx={{ color: 'white' }}/>
+					<img src={saLogo} className='sa-art' alt="saatchi art logo" />
 				</div>
 			</div>
 		</nav>
