9d7acd6393e550693ba9a1890e53a6c20c0170d5
ToriK17 <vfcad@protonmail.com>
2022-05-14 09:27:08 -0600

    adds non breaking space between the social media links

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -17,15 +17,17 @@ const NavBar = () => {
 					<Link to='/about' className='nav-link' >About</Link>
 					<Link to='/videos' className='nav-link' >Videos</Link>
 					<Link to='/contact' className='nav-link' >Contact</Link>
-					<a href='https://www.instagram.com/kafati_art_studio/?hl=en'>
-						<InstagramIcon sx={{ color: 'white' }}/>
-					</a>
-					<a href='https://www.facebook.com/jaime.kafati.5'>
-						<FacebookIcon sx={{ color: 'white' }}/>
-					</a>
-					<a href='https://www.saatchiart.com/kafati'>
-						<img src={saLogo} className='sa-art' alt="saatchi art logo" />
-					</a>
+					<div className='media-links'>
+						<a href='https://www.instagram.com/kafati_art_studio/?hl=en'>
+							<InstagramIcon sx={{ color: 'white' }}/>
+						</a>&nbsp;
+						<a href='https://www.facebook.com/jaime.kafati.5'>
+							<FacebookIcon sx={{ color: 'white' }}/>
+						</a>&nbsp;
+						<a href='https://www.saatchiart.com/kafati'>
+							<img src={saLogo} className='sa-art' alt="saatchi art logo" />
+						</a>
+					</div>
 				</div>
 			</div>
 		</nav>
