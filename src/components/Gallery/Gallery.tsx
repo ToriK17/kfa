d9cc748501c13b15c5573d6fd07f6f90700a38d0
ToriK17 <vfcad@protonmail.com>
2022-08-13 06:34:57 -0600

    renders and maps data from contentful API

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
 import {useState, useEffect} from "react";
 import './gallery.scss'
-// import Card from '@mui/material/Card';
-// import CardContent from '@mui/material/CardContent';
-// import CardMedia from '@mui/material/CardMedia';
-// import Typography from '@mui/material/Typography';
-// import galleryData from './gallery_data.js';
-
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
 const Gallery = () => {
 	 // define the initial state
-	interface image {
+	interface data {
 		title?: string
 		dimension?: string
 		mediaType?: string
@@ -18,8 +16,7 @@ const Gallery = () => {
 		dataObj?: any
 	}
 
-	const [image, setImage] =  useState<any>([])
-	const [dataObj, setValue] =  useState<any>([])
+	const [data, setData] = useState<any>([]);
 	const query = `
 	{
 		paintingPostCollection {
@@ -42,7 +39,8 @@ const Gallery = () => {
 	}
 	`
 	useEffect(() => {
-    window.fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_SPACE_ID}/`, {
+    window
+      .fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_SPACE_ID}/`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -53,64 +51,44 @@ const Gallery = () => {
         body: JSON.stringify({ query }),
       })
       .then((response) => response.json())
-      .then(({ data, errors }) => {
-        if (errors) {
-          // console.error(errors);
-        }
-
-        // rerender the entire component with new data
-				// debugger
-
-				console.log(data.paintingPostCollection.items[0].title)
-				// setValue(data.paintingPostCollection.items[0].dimensions) works
-				setValue(data.paintingPostCollection)
-				// return data.paintingPostCollection.items[0].title
-        // setImage(data.paintingPostCollection.items[0]);
-				setImage(data.paintingPostCollection.items[0].imageFile.url)
-
-
+			.then((data) => {
+        setData(data.data.paintingPostCollection.items)
+      })
+			.catch((err) => {
+        console.log(err);
       });
   }, []);
 
-
-	const galleryObj:any = dataObj.items
-	console.log(galleryObj.map((galleryObj:any) => galleryObj))
-
+	let galleryObj = data
+	console.log(data.map((g:any) => g) || {})
 
 	return (
 		<>
 		<div className='title'>
-			GALLERY
 		</div>
-		{/* <div className='banner'></div>	 */}
-		{/* {!dataObj && 'LOADING'} */}
-		{/* <p>{dataObj!.title || ""}</p> */}
 		<div className='card-layout'>
-			{/* {galleryObj} */}
-			{/* <img src = {image} alt = "painting"/> */}
-			{/* {galleryObj.map((data:any) => {
+			{galleryObj.map((data:any) => {
 				return (
 					<div className='card-container'>
 						<Card sx={{ maxWidth: 345 }}>
 							<CardMedia
 								component="img"
-								alt={data.alt}
-								height={data.height}
-								image={data.image}
-								key={'key' + data.title}
+								alt={data.imageFile.description}
+								image={data.imageFile.url}
+								key={'key' + data.mediaType}
 							/>
 							<CardContent>
-								<Typography variant="subtitle1" component="div">
+								<Typography variant="subtitle1" component="div" key={'key' + data.title}>
 									{data.title}
 								</Typography>
-								<Typography variant="body2" color="text.secondary">
-									{data.dimensions}
+								<Typography variant="body2" color="text.secondary" key={'key' + data.dimensions}>
+									{data.mediaType + "   " + data.dimensions}
 								</Typography>
 							</CardContent>
 						</Card>
 					</div>
 				)
-			})} */}
+			})}
 		</div>
 		</>
 	);
