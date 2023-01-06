6c43be1fb3a5664fabc80a241ff44cae87dda337
ToriK17 <vfcad@protonmail.com>
2023-01-06 16:34:55 -0700

    adds generated video imports

--- src/components/Videos/Videos.tsx
+++ src/components/Videos/Videos.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import './videos.scss';
+import videoList from './video_list';
 
 const Videos = () => {
 
 	return (
 		<section>
 			<h2>Please Visit &nbsp;<a href='https://www.youtube.com/channel/UCzz1Z-gsHb9w4bvj3Xh9REg'>Youtube</a> &nbsp;for full playlist and to Subscribe</h2>
-			<iframe
-				title="youtube video"
-				width="560"
-				height="315"
-				src="https://www.youtube.com/embed/PmjtlK9fWoI"
-				frameBorder="0"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-				allowFullScreen></iframe>
+
+			{videoList.map((data) => {
+				return (
+					<iframe
+						title={data.title}
+						width="560"
+						height="315"
+						src={data.src}
+						key={data.src + data.title}
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
+					</iframe>
+				)
+			})}
+
 		</section>
 	)
 
---
+++ src/components/Videos/video_list.js
@@ -0,0 +1,76 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export default[
+	{
+		src: 'https://www.youtube.com/embed/PmjtlK9fWoI',
+		title: 'youtube home video'
+	},
+	{
+		src: 'https://www.youtube.com/embed/gCI37evP1uA',
+		title: 'Tribunal Mexicano'
+	},
+	{
+		src: 'https://www.youtube.com/embed/LLLU4LfGhy8',
+		title: 'Simon Looking On'
+	},
+	{
+		src: 'https://www.youtube.com/embed/l6HZgSybDjc',
+		title: 'Ayame'
+	},
+	{
+		src: 'https://www.youtube.com/embed/fWQPg27VR60',
+		title: 'Bring US Veterans Home'
+	},
+	{
+		src: 'https://www.youtube.com/embed/ga2YqkoMN1c',
+		title: 'No Kings in Hell'
+	},
+	{
+		src: 'https://www.youtube.com/embed/DGxKAJ0x438',
+		title: 'Beto'
+	},
+	{
+		src: 'https://www.youtube.com/embed/oKBYiauPRE0',
+		title: 'Eve and the Serpent'
+	},
+	{
+		src: 'https://www.youtube.com/embed/MdGYbmQEFqo',
+		title: 'Vanitas'
+	},
+	{
+		src: 'https://www.youtube.com/embed/iPfzZ8BEGA0',
+		title: 'Outside of Paradise'
+	},
+	{
+		src: 'https://www.youtube.com/embed/9a3uQXRSsPM',
+		title: 'My Glazing and Texture Building Process (Portrait of the man from Saipan)'
+	},
+	{
+		src: 'https://www.youtube.com/embed/-LWxG-xULlU',
+		title: 'My Palestinian Great Grandmother'
+	},
+	{
+		src: 'https://www.youtube.com/embed/ICWTiR4kpOc',
+		title: 'Private Wine Label'
+	},
+	{
+		src: 'https://www.youtube.com/embed/MJ5tFe_yVn8',
+		title: 'Lookout Mountain'
+	},
+	{
+		src: 'https://www.youtube.com/embed/0lL6eAYtsmM',
+		title: 'A Tribute to Shireen'
+	},
+	{
+		src: 'https://www.youtube.com/embed/jgDllYsX24o',
+		title: 'No stress Alla Prima'
+	},
+	{
+		src: 'https://www.youtube.com/embed/3M0CB6yrPmU',
+		title: 'Bathsheba'
+	},
+	{
+		src: 'https://www.youtube.com/embed/vwH3zQ6ZzNw',
+		title: 'Thin to Fat Demo'
+	}
+]
